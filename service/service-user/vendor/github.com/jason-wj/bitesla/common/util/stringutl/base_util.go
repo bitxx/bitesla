d20a85bbb72bfe6b05927800f44b13e680e262ab
idea_wj <idea_wj@163.com>
2019-02-17 13:28:43 +0800

    加速更新中

--- service/service-user/vendor/github.com/jason-wj/bitesla/common/util/stringutl/base_util.go
+++
@@ -1,9 +0,0 @@
-package stringutl
-
-import "regexp"
-
-func VerifyEmailFormat(email string) bool {
-	pattern := `\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*` //匹配电子邮箱
-	reg := regexp.MustCompile(pattern)
-	return reg.MatchString(email)
-}
